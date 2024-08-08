d8bcd53d2b0352e2bd757746aa72d07abcae7767
Pablo <pabloalvaradovazquez10@gmail.com>
2024-08-08 17:51:48 -0600

    up

--- Acciones.py
+++ Acciones.py
@@ -6,38 +6,93 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-import json
 
+class Accion:
 
-class Acciones:
-    
-    
-    def hacerClick(self,driver, wait, action, site_name):
+    def hacerClick(self, driver, wait, accion, pagina):
         try:
-            
-            print(f"Clicking on {site_name}")
-            print(action['selector']['by'])
-            print(action['selector']['value'])
+        
+            print(f"Clicking on {pagina}")
+            print(accion['selector']['by'])
+            print(accion['selector']['value'])
 
-            element = wait.until(EC.element_to_be_clickable((getattr(By, action['selector']['by'].upper()), action['selector']['value'])))
+            element = wait.until(EC.element_to_be_clickable((getattr(By, accion['selector']['by'].upper()), accion['selector']['value'])))
             element.click()
             
         except Exception as e:
-            print(f"Error clicking on {site_name}: {e}")
+            print(f"Error clicking on {pagina}: {e}")
 
-    def extraerDatos(self,driver, wait, action, site_name):
+    def esperarPagina(self, driver, wait, accion, pagina):
         try:
-            time.sleep(5)
-            rows = driver.find_elements(By.CSS_SELECTOR, action['selector']['value'])
-            
-            #row es 
 
-            print(f"Total rows found in {site_name}: {len(rows)}")
+                time.sleep(5)
+            
+                container = driver.find_element(by=getattr(By, accion['container_selector']['by'].upper()), value=accion['container_selector']['value'])
+
+                # Encuentra todos los elementos <li> dentro del contenedor
+                print(container)
+                rows = container.find_elements(by=getattr(By, accion['item_selector']['by'].upper()), value=accion['item_selector']['value'])
+
+
+                print(f"Total rows found in {pagina}: {len(rows)}")
+
+                extracted_data = []
+                for row in rows:
+                    data = {}
+                    for field in accion['fields']:
+                        try:
+                            element = row.find_element(by=getattr(By, field['selector']['by'].upper()), value=field['selector']['value'])
+                            data[field['name']] = element.text.strip() if field['name'] != 'link' else element.get_attribute('href').strip()
+                        except NoSuchElementException:
+                            data[field['name']] = ''
+                    if any(data.values()):
+                        extracted_data.append(data)
+                        print(f"Extracted data: {data}")
+
+                self.save_to_files(extracted_data, pagina)
+
+        except Exception as e:
+            print(f"Error extracting data in {pagina}: {e}")
+        
+    def escribirPalabra(self, driver, wait, accion, pagina):
+        try:
+            print(f"Sending keys to {pagina}")
+            
+
+            element = wait.until(EC.element_to_be_clickable((getattr(By, accion['selector']['by'].upper()), accion['selector']['value'])))
+            element.send_keys(accion['value'])
+            element.send_keys(Keys.ENTER)
+        except Exception as e:
+            print(f"Error sending keys in {pagina}: {e}")
+
+    def hacerScroll(self, driver, wait, accion, pagina):
+        try:
+            if accion['selector']['by'] == 'seconds':
+                time.sleep(accion['selector']['value'])
+            else:
+                wait.until(EC.presence_of_element_located((getattr(By, accion['selector']['by'].upper()), accion['selector']['value'])))
+        except Exception as e:
+            print(f"Error waiting for element in {pagina}: {e}")
+
+    #listo
+    def extraerInfo(self, driver, wait, accion, pagina):
+        try:
+
+            time.sleep(5)
+        
+            container = driver.find_element(by=getattr(By, accion['container_selector']['by'].upper()), value=accion['container_selector']['value'])
+
+            # Encuentra todos los elementos <li> dentro del contenedor
+            print(container)
+            rows = container.find_elements(by=getattr(By, accion['item_selector']['by'].upper()), value=accion['item_selector']['value'])
+
+
+            print(f"Total rows found in {pagina}: {len(rows)}")
 
             extracted_data = []
             for row in rows:
                 data = {}
-                for field in action['fields']:
+                for field in accion['fields']:
                     try:
                         element = row.find_element(by=getattr(By, field['selector']['by'].upper()), value=field['selector']['value'])
                         data[field['name']] = element.text.strip() if field['name'] != 'link' else element.get_attribute('href').strip()
@@ -47,80 +102,48 @@ class Acciones:
                     extracted_data.append(data)
                     print(f"Extracted data: {data}")
 
-            self.guardarArchivo(extracted_data, site_name)
+            self.save_to_files(extracted_data, pagina)
         except Exception as e:
-            print(f"Error extracting data in {site_name}: {e}")
+            print(f"Error extracting data in {pagina}: {e}")
+        
 
-    def extraerTabla(self,driver, wait, action, site_name):
+    def extraerTabla(self, driver, wait, accion, pagina):
+        
         try:
             time.sleep(5)
             html_content = driver.page_source
             soup = BeautifulSoup(html_content, 'html.parser')
-            table = soup.select_one(action['selector']['value'])
+            table = soup.select_one(accion['container_selector']['value'])
             rows = table.find_all('tr')
-
             extracted_data = []
             for row in rows[1:]:
                 cells = row.find_all('td')
                 if cells:
                     data = {}
-                    for field in action['fields']:
+                    for field in accion['fields']:
                         try:
-                            cell = cells[action['fields'].index(field)]
-                            data[field['name']] = cell.get_text(strip=True)
+                            if 'property' in field:
+                                element = driver.find_element_by_css_selector(field['container_selector']['value'])
+                                if field['property'] == 'is_visible':
+                                    data[field['name']] = element.is_displayed()
+                                elif field['property'] == 'is_active':
+                                    data[field['name']] = 'active' in element.get_attribute('class')
+                            else:
+                                cell = cells[accion['fields'].index(field)]
+                                data[field['name']] = cell.get_text(strip=True)
                         except IndexError:
                             data[field['name']] = ''
                     if any(data.values()):
                         extracted_data.append(data)
-                        print(f"Extracted data: {data}")
-
-            self.guardarArchivo(extracted_data, site_name)
+                        print(f"Extracted data from {pagina}: {data}")
+            # Guarda los datos o haz algo con ellos
         except Exception as e:
-            print(f"Error extracting table data in {site_name}: {e}")
+            print(f"Error extracting table data on {pagina}: {e}")
 
-    def hacerScroll(driver, wait, action, site_name):
-        try:
-            settings = action['settings']
-            last_height = driver.execute_script("return document.body.scrollHeight")
-            for i in range(settings['repetitions']):
-                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(settings['wait_time'])
-                new_height = driver.execute_script("return document.body.scrollHeight")
-                print(f"Scrolling {i+1}/{settings['repetitions']}: last_height = {last_height}, new_height = {new_height}")
-                if new_height == last_height:
-                    break
-                last_height = new_height
-                time.sleep(2)
-                for j in range(10):
-                    driver.execute_script("window.scrollBy(0, window.innerHeight / 10);")
-                    time.sleep(2)
-            time.sleep(5)
-        except Exception as e:
-            print(f"Error scrolling in {site_name}: {e}")
-
-    def escribir(driver, wait, action, site_name):
-        try:
-            element = wait.until(EC.presence_of_element_located((getattr(By, action['selector']['by'].upper()), action['selector']['value'])))
-            element.send_keys(action['value'])
-            element.send_keys(Keys.ENTER)
-        except Exception as e:
-            print(f"Error sending keys in {site_name}: {e}")
-
-    def esperar(driver, wait, action, site_name):
-        try:
-            if action['selector']['by'] == 'seconds':
-                time.sleep(action['selector']['value'])
-            else:
-                wait.until(EC.presence_of_element_located((getattr(By, action['selector']['by'].upper()), action['selector']['value'])))
-        except Exception as e:
-            print(f"Error waiting for element in {site_name}: {e}")
-
-    def guardarArchivo(data, description):
-        try:
-            df = pd.DataFrame(data)
-            df.to_csv(f'{description}.csv', index=False)
-            print(f"Data saved to {description}.csv")
-        except Exception as e:
-            print(f"Error saving data to {description}.csv: {e}")
-
-
+    def save_to_files(self,data, description):
+            try:
+                df = pd.DataFrame(data)
+                df.to_csv(f'{description}.csv', index=False)
+                print(f"Data saved to {description}.csv")
+            except Exception as e:
+                print(f"Error saving data to {description}.csv: {e}")
--- main.py
+++ main.py
@@ -1,7 +1,59 @@
-from web_scraper import WebScraper
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+from Acciones import Accion
+
+class WebScraper:
+
+    def __init__(self, config_file):
+        self.config_file = config_file
+        self.driver = None
+        self.wait = None
+        options = Options()
+        options.add_argument("--ignore-certificate-errors")
+        options.add_argument("--ignore-ssl-errors")
+        options.add_argument("--disable-web-security")
+        options.add_argument("--allow-running-insecure-content")
+        self.config = self.cargarJson()
+
+    def cargarJson(self):
+        with open(self.config_file, 'r') as file:
+            return json.load(file)
+
+    def configurarDriver(self):
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=service, options=Options())
+        self.wait = WebDriverWait(self.driver, 20)
+
+    def hacerAcciones(self):
+        accion = Accion()
+        for pagina in self.config['paginas']:
+            self.driver.get(pagina['url'])
+            for accion_config in pagina['acciones']:
+                action_type = accion_config['tipo']
+                print(f"Executing action {action_type} on {pagina['pagina']}")
+                if action_type == 'click':
+                    accion.hacerClick(self.driver, self.wait, accion_config, pagina['pagina'])
+                elif action_type == 'esperar_elemento':
+                    accion.esperarPagina(self.driver, self.wait, accion_config, pagina['pagina'])
+                elif action_type == 'send_keys':
+                    accion.escribirPalabra(self.driver, self.wait, accion_config, pagina['pagina'])
+                elif action_type == 'scroll':
+                    accion.hacerScroll(self.driver, self.wait, accion_config, pagina['pagina'])
+                elif action_type == 'extract':
+                    accion.extraerInfo(self.driver, self.wait, accion_config, pagina['pagina'])
+                elif action_type == 'extract_table':
+                    accion.extraerTabla(self.driver, self.wait, accion_config, pagina['pagina'])
+
+    def quit(self):
+        if self.driver:
+            self.driver.quit()
 
 if __name__ == "__main__":
-    scraper = WebScraper(config_file='config.json')
-    scraper.setup_driver()
-    scraper.execute_actions()
+    scraper = WebScraper(config_file='examen.json')
+    scraper.configurarDriver()
+    scraper.hacerAcciones()
     scraper.quit()
